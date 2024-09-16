e89809a8493030e2058d0f45ba949d13ae1e663f
EmilioMendoza <Ironislife39@gmail.com>
2024-09-15 22:12:26 -0400

    Force commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import network
 import utime
 from servo import Servo
 import time
-from machine import Pin, I2C
+from machine import Pin, I2C, reset
 from ssd1306 import SSD1306_I2C
+from umqtt import MQTTClient
+import network
+import secrets
 
 
 
@@ -12,8 +16,55 @@ sdaPin = Pin(16)
 sclPin = Pin(17)
 i2c = I2C(0, sda=sdaPin, scl=sclPin)
 devices = i2c.scan()
-oled = SSD1306_I2C(128, 64, i2c, addr=devices[0])
+#time.sleep(5)
+if devices == []:
+    reset()
+oled = SSD1306_I2C(128, 64, i2c)
 
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(secrets.SSID, secrets.PASSWORD)
+time.sleep(5)
+print(wlan.isconnected())
+
+mqtt_server = "broker.hivemq.com"
+client_id = "a3l6"
+topic_sub = b"Toggle"
+
+
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, keepalive=7200)
+    client.set_callback(subscribe)
+    client.connect()
+
+
+    print(f"Connected to {mqtt_server} MQTT Broker")
+    return client
+
+def reconnect():
+    print("Failed to connect to the MQTT Broker. Reconnecting...")
+    time.sleep(5)
+    reset()
+
+def subscribe(topic, msg):
+    print(f"New message on topic {topic.decode('utf-8')}")
+    msg = msg.decode('utf-8')
+    print(msg)
+
+    if msg == "on":
+        print("ON")
+    else:
+        print("OFF")
+
+try:
+    client = mqtt_connect()
+except OSError:
+    reconnect()
+
+while True:
+    client.subscribe(topic_sub)
+    time.sleep(1)
 
 def servo_Map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
@@ -38,6 +89,6 @@ def right():
         servo_Angle(i)
         utime.sleep(0.05)
 
-oled.fill(0)
-oled.text("Tom's Hardware", 0, 0)
-oled.show()
+#oled.fill(0)
+#oled.text("Tom's Hardware", 0, 0)
+#oled.show()
---
+++ secrets.py
@@ -0,0 +1,5 @@
+SSID = 'ourwifi'
+PASSWORD = "Basslake123"
+MQTT_URL = "broker.hivemq.com"
+MQTT_USERNAME = "test1"#"emilio" # aurora1
+MQTT_PASSWORD = "Password1" #'?s`"?QIT5E~cB~,zbZ5p'
