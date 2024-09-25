5b2e2567269952f12b6f6f868ef3bcfad7266b87
Emilio Mendoza <ironislife39@gmail.com>
2024-09-24 20:20:18 -0400

    Added frontend:

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    typography: {
+      DEFAULT: {
+        css: {
+          color: undefined,
+        },
+      },
+    },
+  },
+  daisyui: {
+    themes: ["garden"],
+  },
+};
